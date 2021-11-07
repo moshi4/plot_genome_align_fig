79f764cfbe77e08653ea1d1fafea54c28527ed07
moshi <sy.41211@gmail.com>
2021-11-07 18:09:58 +0900

    Update src code

--- plot_genome_align_fig.py
+++ plot_genome_align_fig.py
@@ -3,68 +3,54 @@ import argparse
 import os
 import shutil
 import subprocess as sp
-from glob import glob
 from pathlib import Path
-from typing import Optional
+from typing import List
 
 from Bio import Phylo
 from Bio.Phylo.BaseTree import Tree
 
 
-def main(gbk_dir: Path, outdir: Path, tree_file: Optional[Path]):
+def main(
+    indir: Path,
+    outdir: Path,
+    tree_file: Path,
+    width: int,
+    height: int,
+    tree_width: int,
+):
     """Main function"""
     # Define IN/OUT contents
-    user_gbk_dir = outdir / "user_gbk"
-    os.makedirs(user_gbk_dir, exist_ok=True)
-    shutil.copytree(gbk_dir, user_gbk_dir, dirs_exist_ok=True)
-
+    genome_dir = outdir / "genome_data"
     mauve_outdir = outdir / "mauve"
     xmfa_file = mauve_outdir / "mauve.xmfa"
     bbone_file = mauve_outdir / "mauve.bbone"
+    os.makedirs(genome_dir, exist_ok=True)
     os.makedirs(mauve_outdir, exist_ok=True)
+    shutil.copytree(indir, genome_dir, dirs_exist_ok=True)
 
-    svg_plot_file = outdir / "genoPlotR_genome_align.svg"
-    pdf_plot_file = outdir / "genoPlotR_genome_align.pdf"
+    # Setup progressiveMauve & genoPlotR input data
+    genome_file_list = get_genome_file_list(genome_dir, tree_file)
+    guide_tree_file = mauve_outdir / "guide_tree.nwk"
+    write_guide_tree(tree_file, guide_tree_file)
 
-    # Align genome & plot figure
-    if tree_file:
-        guide_tree_file = outdir / "guide_tree.nwk"
-        tree: Tree = Phylo.read(tree_file, "newick")
-        leaf_name_list = [node.name for node in tree.get_terminals()]
-        gbk_file_list = [
-            str(user_gbk_dir / (leaf_name + ".gbk")) for leaf_name in leaf_name_list
-        ]
-        for cnt, node in enumerate(tree.get_terminals(), 1):
-            node.name = f"seq{cnt}"
-        Phylo.write(tree, guide_tree_file, "newick")
-        # Run progressiveMauve
-        mauve_cmd = (
-            f"progressiveMauve --output={xmfa_file} "
-            + f"--backbone-output={bbone_file} "
-            + f"--input-guide-tree={guide_tree_file} "
-            + f"{' '.join(gbk_file_list)}"
-        )
+    # Run progressiveMauve
+    mauve_cmd = (
+        f"progressiveMauve --output={xmfa_file} "
+        + f"--backbone-output={bbone_file} "
+        + f"--input-guide-tree={guide_tree_file} "
+        + f"{' '.join(genome_file_list)}"
+    )
+    if not bbone_file.exists():
         sp.run(mauve_cmd, shell=True)
-        # Run genoPlotR
-        svg_plot_cmd = f"./run_genoPlotR.R {bbone_file} {svg_plot_file} {tree_file}"
-        sp.run(svg_plot_cmd, shell=True)
-        pdf_plot_cmd = f"./run_genoPlotR.R {bbone_file} {pdf_plot_file} {tree_file}"
-        sp.run(pdf_plot_cmd, shell=True)
 
-    else:
-        gbk_file_list = glob(str(user_gbk_dir) + "/*.gbk")
-        # Run progressiveMauve
-        mauve_cmd = (
-            f"progressiveMauve --output={xmfa_file} "
-            + f"--backbone-output={bbone_file} "
-            + f"{' '.join(gbk_file_list)}"
+    # Run genoPlotR
+    for ext in (".jpg", ".png", ".svg", ".pdf"):
+        plot_file = outdir / ("genoPlotR_genome_align" + ext)
+        plot_cmd = (
+            f"./run_genoPlotR.R {bbone_file} {plot_file} {tree_file} "
+            + f"{width} {height} {tree_width}"
         )
-        sp.run(mauve_cmd, shell=True)
-        # Run genoPlotR
-        svg_plot_cmd = f"./run_genoPlotR.R {bbone_file} {svg_plot_file}"
-        sp.run(svg_plot_cmd, shell=True)
-        pdf_plot_cmd = f"./run_genoPlotR.R {bbone_file} {pdf_plot_file}"
-        sp.run(pdf_plot_cmd, shell=True)
+        sp.run(plot_cmd, shell=True)
 
 
 def get_args() -> argparse.Namespace:
@@ -78,11 +64,11 @@ def get_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        "-g",
-        "--gbk_dir",
+        "-i",
+        "--indir",
         required=True,
         type=Path,
-        help="Input genbank files directory (*.gbk)",
+        help="Input genome Genbank or Fasta files directory (*.gbk|*.fa)",
         metavar="",
     )
     parser.add_argument(
@@ -96,19 +82,86 @@ def get_args() -> argparse.Namespace:
     parser.add_argument(
         "-t",
         "--tree",
+        required=True,
         type=Path,
         help="Newick species tree file",
-        default=None,
+        metavar="",
+    )
+    default_plot_width = 10
+    parser.add_argument(
+        "--width",
+        type=int,
+        default=default_plot_width,
+        help=f"Plot width inch (Default: {default_plot_width})",
+        metavar="",
+    )
+    default_plot_height = 7
+    parser.add_argument(
+        "--height",
+        type=int,
+        default=default_plot_height,
+        help=f"Plot height inch (Default: {default_plot_height})",
+        metavar="",
+    )
+    default_tree_width = 2
+    parser.add_argument(
+        "--tree_width",
+        type=int,
+        default=default_tree_width,
+        help=f"Plot tree width inch (Default: {default_tree_width})",
         metavar="",
     )
 
     return parser.parse_args()
 
 
+def get_genome_file_list(genome_dir: Path, tree_file: Path) -> List[str]:
+    """Get genome file list in genome_dir from tree names
+
+    Args:
+        genome_dir (Path): Genome genbank or fasta files directory
+        tree_file (Path): Newick tree file
+
+    Returns:
+        List[str]: Genome genbank or fasta file list
+    """
+    tree: Tree = Phylo.read(tree_file, "newick")
+    leaf_name_list = [node.name for node in tree.get_terminals()]
+    genome_file_list = []
+    for leaf_name in leaf_name_list:
+        file_prefix = genome_dir / leaf_name
+        gbk_file = Path(f"{file_prefix}.gbk")
+        fasta_file = Path(f"{file_prefix}.fa")
+        if gbk_file.exists():
+            genome_file_list.append(str(gbk_file))
+        elif fasta_file.exists():
+            genome_file_list.append(str(fasta_file))
+        else:
+            err = f"Genome file '{gbk_file}' or '{fasta_file}' not found!!"
+            raise FileNotFoundError(err)
+    return genome_file_list
+
+
+def write_guide_tree(tree_file: Path, guide_tree_file: Path) -> None:
+    """Write guide tree for progressiveMauve
+
+    Args:
+        tree_file (Path): Input newick tree file
+        guide_tree_file (Path): Output newick guide tree file
+    """
+    tree: Tree = Phylo.read(tree_file, "newick")
+    for cnt, node in enumerate(tree.get_terminals(), 1):
+        node.name = f"seq{cnt}"
+    Phylo.write(tree, guide_tree_file, "newick", plain=True)
+
+
 if __name__ == "__main__":
     args = get_args()
     main(
-        args.gbk_dir,
+        args.indir,
         args.outdir,
         args.tree,
+        args.width,
+        args.height,
+        args.tree_width,
     )
--- run_genoPlotR.R
+++ run_genoPlotR.R
@@ -9,47 +9,46 @@ args <- commandArgs(TRUE)
 bbone_file <- args[1]
 plot_file <- args[2]
 tree_file <- args[3]
+width <- as.numeric(args[4])
+height <- as.numeric(args[5])
+tree_width <- as.numeric(args[6])
 
 # Load progressiveMauve result
 bbone <- read_mauve_backbone(
-	bbone_file,
+    bbone_file,
     ref=1,
     filter_low=0,
-	common_blocks_only=TRUE,
+    common_blocks_only=TRUE,
 )
 
-# Define plot format (PDF or SVG)
+# Load newick tree file (Delete node labels)
+tree <- read.tree(tree_file)
+tree$node.label <- NULL
+tree <- newick2phylog(write.tree(tree))
+
+# Setup graphics device driver (PDF or SVG)
 ext <- file_ext(plot_file)
 if (ext == "pdf") {
-    pdf(plot_file, onefile=TRUE)
+    pdf(plot_file, onefile=TRUE, width=width, height=height)
 } else if (ext == "svg") {
-    svg(plot_file, onefile=TRUE)
+    svg(plot_file, onefile=TRUE, width=width, height=height)
+} else if (ext == "jpg") {
+    jpeg(plot_file, width=width, height=height, units="in", res=300)
+} else if (ext == "png") {
+    png(plot_file, width=width, height=height, units="in", res=300)
 }
 
-gene_type <- "side_blocks" # arrows
-
-if (is.na(tree_file)) {
-    tree = NULL
-    labels = NULL
-} else {
-    tree <- newick2phylog(write.tree(read.tree(tree_file)))
-    labels = names(tree$leaves)
-}
-
-# treed <- ifelse(missing(tree_file), NULL, newick2phylog(readLines(file(tree_file, "r"), 1)))
-# print(tree)
-
+# Plot genome alignment figure
 plot_gene_map(
     dna_segs=bbone$dna_segs,
     comparisons=bbone$comparisons,
     tree=tree,
-    dna_seg_labels = labels,
-    gene_type=gene_type,
-    # tree=newick2phylog(readLines(file(tree_file, "r"), 1)),
-    arrow_head_len=Inf,
+    tree_width=tree_width,
+    dna_seg_labels = names(tree$leaves),
+    gene_type="side_blocks",
     xlims = NULL,
-    global_color_scheme=c("auto", "auto", "grey", 0.5),
+    global_color_scheme=c("auto", "auto", "grey", 0.7),
     override_color_schemes=TRUE,
     dna_seg_scale=FALSE,
-    scale=FALSE,
+    scale=TRUE,
 )
